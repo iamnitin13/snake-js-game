cff5b420a42176f898864e421738fb52a1d5b6ef
Nitin Srivastava <nitins1bro@gmail.com>
2024-01-05 05:10:52 +0530

    feat: completed snake game

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const grid = document.querySelector(".grid-cells");
-const myScore = document.querySelector("#my-score");
+const myScore = document.querySelector("#my-score > b");
+const highScore = document.querySelector("#highest-score > b");
 const startBtn = document.querySelector(".start-btn");
-const highestScore = JSON.parse(localStorage.getItem("highScore")) || 0;
 
 let currentSnake = [2, 1, 0];
 let direction = 1;
 let intervalTime = 200;
 let interval = 0;
+let prevFoodPos = -1;
 const width = 20;
 let gameStarted = false;
+let numCells = width * width;
+let score = 0;
+let topScore = JSON.parse(localStorage.getItem("highScore")) || 0;
 
-for (let row = 0; row < width * width; row++) {
+for (let row = 0; row < numCells; row++) {
   const gridCell = document.createElement("div");
   gridCell.classList.add("cell");
   grid.appendChild(gridCell);
@@ -20,18 +24,19 @@ const cells = document.querySelectorAll(".grid-cells div");
 
 // on document load create the board
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll("#higest-score b").innerHTML = highestScore;
-  createBoard();
+  gameBoardSetup();
 });
 
-function createBoard() {
-  cells[currentSnake[0]].innerHTML = "👀";
-  currentSnake.forEach((i) => {
-    cells[i].classList.add("snake");
-  });
-  foodPosition();
+function gameBoardSetup() {
+  currentSnake = [2, 1, 0];
+  score = 0;
+  currentSnake.forEach((i) => cells[i].classList.add("snake"));
+  setFoodPosition();
   clearInterval(interval);
   direction = 1;
+  gameStarted = false;
+  myScore.textContent = score;
+  highScore.textContent = topScore;
 }
 
 function startGame() {
@@ -42,41 +47,95 @@ function startGame() {
 }
 
 function gameLoop() {
-  // remove tail and eye from head position
-  cells[currentSnake[0]].innerText = "";
+  if (detectCollison()) return;
   const tail = currentSnake.pop();
   cells[tail].classList.remove("snake");
   currentSnake.unshift(currentSnake[0] + direction);
-  // add eyes and change snake head position color
+  detectFoodEaten(tail);
   cells[currentSnake[0]].classList.add("snake");
-  cells[currentSnake[0]].innerText = "👀";
 }
 
-function foodPosition() {
-  const randomPosition = getRandomValue(cells);
-  cells[randomPosition].innerHTML = "🐸";
+function detectCollison() {
+  if (
+    (currentSnake[0] + width >= numCells && direction === width) || // hits bottom wall
+    (currentSnake[0] % width === width - 1 && direction === 1) || // hits right wall
+    (currentSnake[0] % width === 0 && direction === -1) || // hits left wall
+    (currentSnake[0] - width < 0 && direction === -width) || // hits the top wall
+    cells[currentSnake[0] + direction].classList.contains("snake") // hits itself
+  ) {
+    clearInterval(interval);
+    setTimeout(() => reset(), 100);
+    return true;
+  }
 }
 
-function getRandomValue(arr) {
-  return Math.floor(Math.random() * arr.length);
+function detectFoodEaten(tail) {
+  if (cells[currentSnake[0]].classList.contains("food-item")) {
+    cells[currentSnake[0]].classList.remove("food-item");
+    cells[tail].classList.add("snake");
+    currentSnake.push(tail);
+    score++;
+    myScore.textContent = score;
+    if (score > +highScore.textContent) {
+      highScore.textContent = score;
+    }
+    cells[prevFoodPos].textContent = "";
+    clearInterval(interval);
+    intervalTime -= 10;
+    interval = setInterval(gameLoop, Math.max(intervalTime, 50));
+    setFoodPosition();
+  }
 }
 
-function moveSnake(moves) {
-  switch (moves) {
-    case "ArrowRight":
-      direction = 1;
-      break;
-    case "ArrowLeft":
-      direction = -1;
-      break;
-    case "ArrowDown":
-      direction = width;
-      break;
-    case "ArrowUp":
-      direction = -width;
-      break;
-    default:
-      break;
+function reset() {
+  const myscore = +myScore.textContent;
+  alert(`Your Score is : ${myscore}`);
+  startBtn.style.pointerEvents = "auto";
+  startBtn.removeAttribute("disabled");
+  cells[prevFoodPos].textContent = "";
+  currentSnake.forEach((i) => cells[i].classList.remove("snake"));
+  topScore = Math.max(myscore, topScore);
+  localStorage.setItem("highScore", topScore);
+  gameBoardSetup();
+}
+
+async function setFoodPosition() {
+  const randomPosition = Math.floor(Math.random() * numCells);
+  if (currentSnake.includes(randomPosition)) {
+    await wait(100);
+    setFoodPosition();
+  } else {
+    prevFoodPos = randomPosition;
+    cells[randomPosition].textContent = "🐸";
+    cells[randomPosition].classList.add("food-item");
+  }
+}
+
+function wait(ms = 0) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function moveSnake(moveDirection) {
+  let directionVal;
+  if (moveDirection === "ArrowRight" && direction !== -1) {
+    directionVal = 1;
+    if (currentSnake[0] + directionVal === currentSnake[1]) return;
+    direction = directionVal;
+  }
+  if (moveDirection === "ArrowLeft" && direction !== 1) {
+    directionVal = -1;
+    if (currentSnake[0] + directionVal === currentSnake[1]) return;
+    direction = directionVal;
+  }
+  if (moveDirection === "ArrowUp" && direction !== width) {
+    directionVal = -width;
+    if (currentSnake[0] + directionVal === currentSnake[1]) return;
+    direction = directionVal;
+  }
+  if (moveDirection === "ArrowDown" && direction !== -width) {
+    directionVal = width;
+    if (currentSnake[0] + directionVal === currentSnake[1]) return;
+    direction = directionVal;
   }
 }
 
